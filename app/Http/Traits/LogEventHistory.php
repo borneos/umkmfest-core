25ad2509df952de042ee76a4bcffff322bddf5fc
fsvhino <ferrysafrizal20@gmail.com>
2023-12-06 22:23:16 +0800

    no message

--- app/Http/Traits/LogEventHistory.php
+++ app/Http/Traits/LogEventHistory.php
@@ -17,13 +17,13 @@ trait LogEventHistory
         $category = $data['category'];
 
         if ($category == null) {
-            return ModelsLogEventHistory::orwhere('telp', '=', $telp)
+            return ModelsLogEventHistory::where('telp', '=', $telp)
                 ->orwhere('email', '=', $email)
                 ->orderBy('id', $sort)
                 ->get();
         } else {
-            return ModelsLogEventHistory::orwhere('telp', '=', $telp)
-                ->orwhere('event_category', '=', $category)
+            return ModelsLogEventHistory::where('telp', '=', $telp)
+                ->where('event_category', '=', $category)
                 ->orderBy('id', $sort)
                 ->get();
         }
