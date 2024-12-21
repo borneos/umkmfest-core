9b36db2efc15e3ccb6ef333253d5e3f260d5fa7e
Agung Kurniawan <agung41149@gmail.com>
2024-12-21 16:46:40 +0800

    - uuid

--- database/migrations/2024_12_21_164531_new_prefix_uuid_log_event_histories_table.php
+++ database/migrations/2024_12_21_164531_new_prefix_uuid_log_event_histories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class NewPrefixLogEventHistoriesTable extends Migration
+class NewPrefixUuidLogEventHistoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -26,7 +26,7 @@ class NewPrefixLogEventHistoriesTable extends Migration
     public function down()
     {
         Schema::table('log_event_histories', function (Blueprint $table) {
-            $table->dropColumn('prefix');
+            //
         });
     }
 }
